bdb9ea728de2037c1b375249a278ee7cbb6b692a
Léa M <mhoumadi.lea@gmail.com>
2022-03-14 22:32:33 +0100

    tool menu en cours, alignements à faire

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 let fileToggleMenu = document.querySelector('.file-menu-scroll');
+let shapeToggleMenu = document.querySelector('.shape')
 let body = document.querySelector('body');
 
 fileToggleMenu.addEventListener('click', function(){
-    body.classList.toggle('open');
+    body.classList.toggle('open-file');
+})
+shapeToggleMenu.addEventListener('click', function(){
+    body.classList.toggle('open-shape');
 })
